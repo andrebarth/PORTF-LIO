550d814841090694c371235fca16f4ea1c407545
andrebarth <a.barth@escola.pr.gov.br>
2025-04-10 08:00:27 -0300

    2

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-document.addEventListener('DOMContentLoaded', function (){
-    cost aumentaFonteBotao = document.getElementByld('aumentar-fonte');
-    cost diminuiFonteBotao = document.getElementByld('diminuir-fonte');
+document.addEventListener('DOMContentLoaded', function () {
 
-let tamanhoAtualFonte = 1;
-
-aumentalFonteBotao.addEventListener('click', function(){
-tamanhoAtualFonte+= 0.1;
-document.body.style.fontSize = '${tamanhoAtualFonte}rem'
-
-})
-
-diminuiFonteBotao.addEventListener('click', function(){
-tamanhoAtualFonte+= 0.1;
-document.body.style.fontSize = '${tamanhoAtualFonte}rem'
-
-})
-
-})
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+ 
+    let tamanhoAtualFonte = 1;
+ 
+    aumentaFonteBotao.addEventListener('click', function () {
+         tamanhoAtualFonte += 0.1;
+         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+ 
+    })
+ 
+    diminuiFonteBotao.addEventListener('click', function () {
+         tamanhoAtualFonte -= 0.1;
+         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+ 
+    })
+ 
+ })
